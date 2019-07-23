93a2c8122b0037f2e1600ca37c8ff238209da66a
Armandot1998 <armandotcajilema@gmail.com>
2019-07-22 20:25:21 -0500

    product

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -21,32 +21,22 @@ class ProductsController extends Controller
             'product_name'=>$dataCategory['product_name'],
             'category_name'=>$dataCategory['category_name'],
             'cost'=>$dataCategory['cost']]);
-            
+
             return 'Operation Sucssesfull!!';     
     }
 
-    public function updateProducts(Request $request)
-    {
-        $dataBodyClient= $request->json()->all();
-        $dataProduct= $dataBodyClient['product'];
-        $dataCategory= $dataBodyClient['category'];
-        $response = Product::findorfail($dataProduct['id']);
-        $response->update(['nombre' => $dataProduct['nombre'],
-        'precio' => $dataProduct['precio'],
-        'stock' => $dataProduct['stock']]);
-        return  response()->json([$response->category_id,$response->nombre], 200);
-        //    return response()->json(['message' => 'Producto actualizado correctamente id= '.$request->id], 200);
+    public function putProduct(Request $request){
+        $dataBodyClient = $request->json()->all();
+        $dataCategory = $dataBodyClient['Category']; 
+        $category = Category::find($dataCategory['id']);
+        $category->update($request->all());
+
+        return 'Operation Sucssesfull!!'; 
     }
 
-    public function deleteProduct(Request $request)
-    {
-        //$product = Product::where('id', '>=', $request->id)->delete();
+    public function deleteProduct(Request $request) {
         $product = Product::findorfail($request->id)->delete();
-        return response()->json(['estado' => $product], 200);
-    }
-    public function deleteProductId($id)
-    {
-        $product = Product::where('id', '=', $id)->delete();
-        return response()->json(['message' => 'Producto eliminado correctamente por Id'], 200);
+
+         return 'Operation Sucssesfull!!'; 
     }
 }
